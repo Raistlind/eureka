6eba69844a8bc5a3bb04bbf426c66ac5b88b8371
RaistlinD <Johnd0712@gmail.com>
2018-06-27 23:26:22 +0800

    ok

---
+++ src/main/java/cn/krynn/eureka/EurekaApplication.java
@@ -0,0 +1,13 @@
+package cn.krynn.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Enable
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
